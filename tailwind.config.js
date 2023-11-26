1ce7a4db152d4a281590b09cabe4293e2c059da4
Syahrul <syahrul4w@gmail.com>
2023-11-26 16:53:11 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,php,html,css,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
