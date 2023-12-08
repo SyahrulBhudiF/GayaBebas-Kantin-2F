1f3d71c2269e82c0097373caf8fc7a6320b0adfc
Syahrul <syahrul4w@gmail.com>
2023-12-08 17:12:01 +0700

    add dummy data for all component

--- app/views/admin/pages/laporan.php
+++ app/views/admin/pages/laporan.php
@@ -1,3 +1,12 @@
+<?php
+// Contoh data
+$data_penjualan = array(
+    array("No" => 1, "Nama Operator" => "Budi Setyaningrum", "Nama Barang" => "Citato", "Jumlah Barang" => 123, "Total Penjualan" => "Rp 1.500.000", "Tanggal Transaksi" => "26 Oktober 2023"),
+    array("No" => 2, "Nama Operator" => "Andi Saputra", "Nama Barang" => "Pocari Sweat", "Jumlah Barang" => 75, "Total Penjualan" => "Rp 800.000", "Tanggal Transaksi" => "27 Oktober 2023"),
+    array("No" => 3, "Nama Operator" => "Citra Indah", "Nama Barang" => "Indomie Goreng", "Jumlah Barang" => 200, "Total Penjualan" => "Rp 2.000.000", "Tanggal Transaksi" => "28 Oktober 2023"),
+);
+
+?>
 <section class="flex flex-col fadeIn p-4 gap-2 w-full h-screen">
     <div class="flex flex-col bg-Neutral/10 rounded-[1.25rem] p-6 gap-6 h-[87%] laptop2:h-[85%]">
         <div class="flex justify-between w-full">
@@ -31,48 +40,26 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php foreach ($data_penjualan as $data) : ?>
                     <tr>
-                        <td class="tableContent text-Neutral/60">01</td>
-                        <td class="tableContent">Budi Setyaningrum</td>
-                        <td class="tableContent">Citato</td>
-                        <td class="tableContent">123</td>
-                        <td class="tableContent">Rp 1.500.000</td>
-                        <td class="tableContent">26 Oktober 2023</td>
+                        <td class="tableContent text-Neutral/60"><?php echo $data['No']; ?></td>
+                        <td class="tableContent"><?php echo $data['Nama Operator']; ?></td>
+                        <td class="tableContent"><?php echo $data['Nama Barang']; ?></td>
+                        <td class="tableContent"><?php echo $data['Jumlah Barang']; ?></td>
+                        <td class="tableContent"><?php echo $data['Total Penjualan']; ?></td>
+                        <td class="tableContent"><?php echo $data['Tanggal Transaksi']; ?></td>
                     </tr>
-                    <tr>
-                        <td class="tableContent text-Neutral/60">02</td>
-                        <td class="tableContent">Budi Setyaningrum</td>
-                        <td class="tableContent">Citato</td>
-                        <td class="tableContent">123</td>
-                        <td class="tableContent">Rp 1.500.000</td>
-                        <td class="tableContent">26 Oktober 2023</td>
-                    </tr>
-                    <tr>
-                        <td class="tableContent text-Neutral/60">03</td>
-                        <td class="tableContent">Budi Setyaningrum</td>
-                        <td class="tableContent">Citato</td>
-                        <td class="tableContent">123</td>
-                        <td class="tableContent">Rp 1.500.000</td>
-                        <td class="tableContent">26 Oktober 2023</td>
-                    </tr>
-                    <tr>
-                        <td class="tableContent text-Neutral/60">04</td>
-                        <td class="tableContent">Budi Setyaningrum</td>
-                        <td class="tableContent">Citato</td>
-                        <td class="tableContent">123</td>
-                        <td class="tableContent">Rp 1.500.000</td>
-                        <td class="tableContent">26 Oktober 2023</td>
-                    </tr>
+                    <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
         <!-- End Table -->
 </section>
 <script>
-    function toggleCheckedClass(label) {
-        document.querySelectorAll('label.inputRadioLapor').forEach(label => label.classList.remove('checked'));
-        if (label.control.checked) {
-            label.classList.add('checked');
-        }
+function toggleCheckedClass(label) {
+    document.querySelectorAll('label.inputRadioLapor').forEach(label => label.classList.remove('checked'));
+    if (label.control.checked) {
+        label.classList.add('checked');
     }
+}
 </script>
